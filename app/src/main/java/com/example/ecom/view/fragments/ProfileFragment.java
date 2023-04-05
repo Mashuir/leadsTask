9472fe250166c7ef3d92aead78d0bb9d99576d96
Sajib <mashuir3@gmail.com>
2023-04-05 16:12:36 +0600

    add camera launcher

--- app/src/main/java/com/example/ecom/view/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/ecom/view/fragments/ProfileFragment.java
@@ -5,21 +5,18 @@ import static android.content.Context.MODE_PRIVATE;
 
 import android.Manifest;
 import android.app.Dialog;
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
@@ -35,6 +32,8 @@ import android.widget.Toast;
 import com.example.ecom.R;
 import com.example.ecom.databinding.FragmentProductDetailsBinding;
 import com.example.ecom.databinding.FragmentProfileBinding;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.io.IOException;
 import java.util.HashSet;
@@ -45,8 +44,9 @@ public class ProfileFragment extends Fragment {
 
     FragmentProfileBinding binding;
 
+    ActivityResultLauncher<String> requestPermissionLauncher;
+    ActivityResultLauncher<Intent> capturePictureLauncher;
     private ActivityResultLauncher<String> galleryLauncher;
-
 
     public ProfileFragment() {
     }
@@ -56,6 +56,8 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         binding = FragmentProfileBinding.inflate(inflater, container, false);
         return binding.getRoot();
+
+
     }
 
     @Override
@@ -65,12 +67,33 @@ public class ProfileFragment extends Fragment {
         SharedPreferences sharedPreferences = requireActivity().getSharedPreferences("MyPrefs", MODE_PRIVATE);
         Set<String> productIDs = sharedPreferences.getStringSet("Items", new HashSet<>());
 
-        for (String productId : productIDs) {
-            // Do something with the product ID
-            Toast.makeText(requireContext(), ""+productId, Toast.LENGTH_SHORT).show();
-        }
+        binding.chnageImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                for (String productId : productIDs) {
+                    // Do something with the product ID
+                    Toast.makeText(requireContext(), "" + productId, Toast.LENGTH_SHORT).show();
+                }
 
+            }
+        });
+
+        requestPermissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+            if (isGranted) {
+                capturePicture();
+            } else {
+                Snackbar.make(binding.rootProfileLayout, "Require camera permission", BaseTransientBottomBar.LENGTH_SHORT).show();
+            }
+        });
+
+        capturePictureLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+            if (result.getResultCode() == RESULT_OK) {
+                assert result.getData() != null;
+                Bitmap imageBitmap = (Bitmap) result.getData().getExtras().get("data");
+                binding.profileCircleImage.setImageBitmap(imageBitmap);
+            }
+        });
 
         galleryLauncher = registerForActivityResult(new ActivityResultContracts.GetContent(), uri -> {
             if (uri != null) {
@@ -91,7 +114,15 @@ public class ProfileFragment extends Fragment {
             TextView pickFromCamera = dialog.findViewById(R.id.pickFromCamera);
             TextView pickFromGallery = dialog.findViewById(R.id.pickFromGallery);
 
-            pickFromCamera.setOnClickListener(v12 -> dialog.dismiss());
+            pickFromCamera.setOnClickListener(v12 -> {
+                if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissionLauncher.launch(Manifest.permission.CAMERA);
+                } else {
+                    // Permission is already granted, capture a picture
+                    capturePicture();
+                }
+                dialog.dismiss();
+            });
 
             pickFromGallery.setOnClickListener(v1 -> {
                 galleryLauncher.launch("image/*");
@@ -105,7 +136,9 @@ public class ProfileFragment extends Fragment {
         });
     }
 
-
-
+    private void capturePicture() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        capturePictureLauncher.launch(intent);
+    }
 
 }
